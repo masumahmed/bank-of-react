898c433ed54fef157943e2a8f3b61a37191305fa
MasumAhmed <MasumAhmed1798@gmail.com>
2021-07-02 09:55:48 -0400

    Credit and Debit added
    - credit and debit pages were populated with API fetch JSON info

--- src/Credit.js
+++ src/Credit.js
@@ -1,17 +1,48 @@
-import React, {Component} from 'react';
-import Nav from './components/Nav';
-import Footer from './components/Footer';
+import React, { Component } from "react";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
 
 export class Credit extends Component {
-    render(){
-        return(
-            <div>
-                <Nav></Nav>
-                <div id="main"></div>
-                <Footer></Footer>
-            </div>
-        );
+  componentDidMount() {
+    fetch("https://moj-api.herokuapp.com/credits")
+      .then((response) => response.json())
+      .then((data) => this.displayAPI(data));
+  }
+
+  displayAPI(data) {
+    for (let i = 0; i < Object.keys(data).length; i++) {
+      document.querySelector("#creditList").innerHTML +=
+        "ID: " +
+        data[i].id +
+        "<br/>" +
+        "Description: " +
+        data[i].description +
+        "<br/>" +
+        "Amount: $" +
+        data[i].amount +
+        "<br/>" +
+        "Date: " +
+        data[i].date +
+        "<br/><hr/>";
     }
-};
+  }
+
+  render() {
+    return (
+      <div>
+        <Nav></Nav>
+        <div id="main">
+          <div id="Credit">
+            <h1>Credit</h1>
+            <button onClick={this.displayInput}>Add Credit</button>
+            <div id="input"></div>
+            <div id="creditList"></div>
+          </div>
+        </div>
+        <Footer></Footer>
+      </div>
+    );
+  }
+}
 
 export default Credit;
--- src/Debit.js
+++ src/Debit.js
@@ -1,17 +1,38 @@
-import React, {Component} from 'react';
-import Nav from './components/Nav';
-import Footer from './components/Footer';
+import React, { Component } from "react";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
 
 export class Debit extends Component {
-    render(){
-        return(
-            <div>
-                <Nav></Nav>
-                <div id="main"></div>
-                <Footer></Footer>
-            </div>
-        );
+  componentDidMount() {
+    fetch("https://moj-api.herokuapp.com/debits")
+      .then((response) => response.json())
+      .then((data) => this.displayAPI(data));
+  }
+
+  displayAPI(data) {
+    for(let i=0; i<Object.keys(data).length; i++){
+        document.querySelector("#debitList")
+        .innerHTML += "ID: " + data[i].id + "<br/>"
+        + "Description: " + data[i].description + "<br/>"
+        + "Amount: $" + data[i].amount + "<br/>"
+        + "Date: " + data[i].date + "<br/><hr/>";
     }
-};
+  }
+
+  render() {
+    return (
+      <div>
+        <Nav></Nav>
+        <div id="main">
+          <div id="Debit">
+            <h1>Debit</h1>
+            <div id="debitList"></div>
+          </div>
+        </div>
+        <Footer></Footer>
+      </div>
+    );
+  }
+}
 
 export default Debit;
